e810cd94a2f99bd8ce28b62695dc08187bfff774
Yablunovskiy <Yablnovskiy@gmail.com>
2015-02-26 21:37:50 +0200

    End

---
+++ Yabl_Tach/Yabl_Tach/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Yabl_Tach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WindowHeight = 50;
+            Console.WindowWidth = 100;
+            Сursor A = new Сursor();
+            Move(A);
+        }
+
+        static public void Move(Сursor A)
+        {
+            int Mx, Ml, My;
+            ConsoleKeyInfo key;
+            int Width = 80;
+            int Height = 40;
+            int x = 1;
+            int y = 1;
+            Random rand = new Random();
+            Ml = rand.Next(18, 25);
+            do
+            {
+                Mx = rand.Next(0, Width-1);
+                My = rand.Next(0, Height-1);
+            } while ((Ml + Mx) > Width || (Ml + My) > Height);
+            Rectangl q = new Rectangl(Mx, My, Ml);
+            Ml -= 5;
+            do
+            {
+                Mx = rand.Next(0, Width-1);
+                My = rand.Next(0, Height-1);
+            } while (!Intersectoin(q, Mx, My, Ml) || (Ml + Mx) > Width || (Ml + My) > Height);
+
+            Rectangl q2 = new Rectangl(Mx, My, Ml);
+            q.Border();
+            q2.Border();
+
+            while (true)
+            {
+                Console.Clear();
+                A.BildCurs(x, y);
+                A.PrintAll();
+                q.Border();
+                q2.Border();
+                Beep(ref A, q);
+                Beep(ref A, q2);
+                key = Console.ReadKey();
+                switch (key.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        y--;
+                        if (!A.Perhaps(x, y)) y++;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        ++y;
+                        if (!A.Perhaps(x, y)) y--;
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        --x;
+                        if (!A.Perhaps(x, y)) x++;
+                        break;
+                    case ConsoleKey.RightArrow:
+                        ++x;
+                        if (!A.Perhaps(x, y)) x--;
+                        break;
+                }
+            }
+        }
+
+
+        static public void Beep(ref Сursor A, Rectangl Z)
+        {
+            bool beep = false;
+            for (int i=0; i<A.My.Length; i++)
+            {
+                if (A.My[i].GetX() == Z.A.X && (Z.A.Y <= A.My[i].GetY() && A.My[i].GetY() <= Z.C.Y)) beep = true;
+                if (A.My[i].GetX() == Z.B.X && (Z.B.Y <= A.My[i].GetY() && A.My[i].GetY() <= Z.D.Y)) beep = true;
+                if (A.My[i].GetY() == Z.A.Y && (Z.A.X <= A.My[i].GetX() && A.My[i].GetX() <= Z.B.X)) beep = true;
+                if (A.My[i].GetY() == Z.C.Y && (Z.C.X <= A.My[i].GetX() && A.My[i].GetX() <= Z.D.X)) beep = true;
+                //if ((Z.A.X <= A.My[i].GetX() && A.My[i].GetX() <= Z.B.X) && (Z.A.Y <= A.My[i].GetY() && A.My[i].GetY() <= Z.C.Y))
+                //{
+                //    //A.My[i].SetBC(ConsoleColor.Yellow);
+                //    //A.My[i].SetCC(ConsoleColor.Black);
+                //    beep = true;
+                //    //Console.Beep();
+                //}
+                //else
+                //{
+                //    A.My[i].SetBC(ConsoleColor.Black);
+                //    A.My[i].SetCC(ConsoleColor.White);
+                //}
+            }
+            if (beep) Console.Beep();
+        }
+
+        static public bool Intersectoin(Rectangl Z, int x, int y, int l)
+        {
+            if ((Z.A.X <= x && x <= Z.B.X) && (Z.A.Y <= y && y <= Z.C.Y))
+            {
+                return false;
+            }
+            if ((Z.A.X <= x+l && x+l <= Z.B.X) && (Z.A.Y <= y && y <= Z.C.Y))
+            {
+                return false;
+            }
+            if ((Z.A.X <= x + l && x + l <= Z.B.X) && (Z.A.Y <= y+l && y+l <= Z.C.Y))
+            {
+                return false;
+            }
+            if ((Z.A.X <= x && x <= Z.B.X) && (Z.A.Y <= y+l && y+l <= Z.C.Y))
+            {
+                return false;
+            }
+            else return true;
+        }
+    }
+}
---
+++ Yabl_Tach/Yabl_Tach/Сursor.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yabl_Tach
+{
+    struct Point
+    {
+        int X;
+        int Y;
+        char A;
+        ConsoleColor CurColor;
+        ConsoleColor BackColor;
+
+        public Point(int x, int y, char a, ConsoleColor cC, ConsoleColor bC)
+        {
+            this.X = x;
+            this.Y = y;
+            this.A = a;
+            this.CurColor = cC;
+            this.BackColor = bC;
+        }
+
+        public void SetPoint(int x, int y, char a, ConsoleColor cC, ConsoleColor bC)
+        {
+            this.X = x;
+            this.Y = y;
+            this.A = a;
+            this.CurColor = cC;
+            this.BackColor = bC;
+        }
+
+        public void SetXY(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public void SetX(int x)
+        {
+            this.X = x;
+        }
+
+        public void SetY(int y)
+        {
+            this.Y = y;
+        }
+
+        public void SetCC( ConsoleColor cC)
+        {
+            this.CurColor = cC;
+        }
+
+        public void SetBC( ConsoleColor bC)
+        {
+            this.BackColor = bC;
+        }
+
+        public void SetA( char a)
+        {
+            this.A = a;
+        }
+
+        public char GetA()
+        {
+            return A;
+        }
+
+        public int GetY()
+        {
+            return Y;
+        }
+
+        public int GetX()
+        {
+            return X;
+        }
+
+        public ConsoleColor GetCC()
+        {
+            return CurColor;
+        }
+
+        public ConsoleColor GetBC()
+        {
+            return BackColor;
+        }
+    }
+
+    class Сursor
+    {
+        public Point[] My = new Point[9];
+
+        public Сursor()
+        {
+            My[0] = My[2] = My[4] = My[6] = My[8] = new Point(0, 0, '+', ConsoleColor.DarkBlue, ConsoleColor.Cyan);
+            My[1] = My[7] = new Point(0, 0, '|', ConsoleColor.White, ConsoleColor.Black);
+            My[3] = My[5] = new Point(0, 0, '-', ConsoleColor.White, ConsoleColor.Black);
+        }
+
+        public void SetCursor(int ind, int x, int y, char a, ConsoleColor cC, ConsoleColor bC)
+        {
+            My[ind].SetPoint( x, y, a, cC, bC);
+        }
+
+        public bool Perhaps(int x, int y)
+        {
+            if (x >= 0 && y >= 0 && x + 3 <= Console.WindowWidth && y + 3 <= Console.WindowHeight)
+            {
+                return true;
+            }
+            else return false;
+        }
+
+        public void BildCurs(int x, int y)
+        {
+            My[0].SetXY(x, y);
+            My[1].SetXY(x, y + 1);
+            My[2].SetXY(x, y + 2);
+            My[3].SetXY(x + 1, y);
+            My[4].SetXY(x + 1, y + 1);
+            My[5].SetXY(x + 1, y + 2);
+            My[6].SetXY(x + 2, y);
+            My[7].SetXY(x + 2, y + 1);
+            My[8].SetXY(x + 2, y + 2);
+        }
+
+        public void Print(Point a)
+        {
+            Console.BackgroundColor = a.GetBC();
+            Console.ForegroundColor = a.GetCC();
+            Console.SetCursorPosition(a.GetX(), a.GetY());
+            Console.WriteLine(a.GetA()); 
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public void PrintAll()
+        {
+            for(int i=0; i<My.Length; i++)
+            {
+                Print(My[i]);
+            }
+        }
+
+    }
+
+    struct Poin
+    {
+        public int X;
+        public int Y;
+
+        public int PoinX()
+        {
+            return X;
+        }
+        public int PoinY()
+        {
+            return Y;
+        }
+    }
+
+    struct Rectangl
+    {
+        public Poin A;
+        public Poin B;
+        public Poin C;
+        public Poin D;
+
+        public Rectangl(int x, int y, int l)
+        {
+            A.X = x;
+            A.Y = y;
+            B.X = x + l;
+            B.Y = y;
+            C.X = x;
+            C.Y = y + l;
+            D.X = x + l;
+            D.Y = y + l;
+        }
+
+        public int CoordX(Poin a)
+        {
+            return a.PoinX();
+        }
+        
+        public void Border()
+        {
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Red;
+            for (int i = A.X + 1; i < A.X + (B.X - A.X); i++)
+            {
+                Console.SetCursorPosition(i, A.Y);
+                Console.WriteLine('-');
+                Console.SetCursorPosition(i, A.Y + (B.X - A.X));
+                Console.WriteLine('-');
+            }
+            for (int i = A.Y + 1; i < A.Y + (B.X - A.X); i++)
+            {
+                Console.SetCursorPosition(A.X, i);
+                Console.WriteLine('|');
+                Console.SetCursorPosition(A.X + (B.X - A.X), i);
+                Console.WriteLine('|');
+            }
+            Console.SetCursorPosition(A.X,A.Y);
+            Console.WriteLine('+');
+            Console.SetCursorPosition(B.X, B.Y);
+            Console.WriteLine('+');
+            Console.SetCursorPosition(C.X, C.Y);
+            Console.WriteLine('+');
+            Console.SetCursorPosition(D.X, D.Y);
+            Console.WriteLine('+');
+        }
+
+    }
+}
